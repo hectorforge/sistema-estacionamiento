5ebc064f87b91d9f64c16d12b6c31e0516b0a571
hectorforge <hector.hernandez.dev@gmail.com>
2025-06-11 20:01:28 -0500

    Entidades

--- sistema-estacionamiento-main/src/main/java/com/estacionamiento/entity/Estacionamiento.java
+++ sistema-estacionamiento-main/src/main/java/com/estacionamiento/entity/Estacionamiento.java
@@ -1,5 +1,13 @@
 package com.estacionamiento.entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Estacionamiento {
 	private int idEstacionamiento;
 	private int espacios;
