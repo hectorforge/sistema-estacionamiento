0625d9e854b447558ed47b683152a14e8cf39647
hectorforge <hector.hernandez.dev@gmail.com>
2025-06-11 20:36:47 -0500

    metodos del use case

--- sistema-estacionamiento-main/src/main/java/com/estacionamiento/usecase/EstacionamientoUseCase.java
+++ sistema-estacionamiento-main/src/main/java/com/estacionamiento/usecase/EstacionamientoUseCase.java
@@ -1,5 +1,10 @@
 package com.estacionamiento.usecase;
 
+import com.estacionamiento.entity.Estacionamiento;
+
 public interface EstacionamientoUseCase {
-	
+    Estacionamiento consultarEstadoActual();
+    void asignarEspacio();
+    void liberarEspacio();
+    Estacionamiento obtenerEstacionamientoActivo();
 }
--- sistema-estacionamiento-main/src/main/java/com/estacionamiento/usecase/VehiculoUseCase.java
+++ sistema-estacionamiento-main/src/main/java/com/estacionamiento/usecase/VehiculoUseCase.java
@@ -1,5 +1,8 @@
 package com.estacionamiento.usecase;
 
+import com.estacionamiento.entity.Vehiculo;
+
 public interface VehiculoUseCase {
-	
+    Vehiculo registrarIngreso(Vehiculo vehiculo);
+    double registrarSalida(int idVehiculo);
 }
