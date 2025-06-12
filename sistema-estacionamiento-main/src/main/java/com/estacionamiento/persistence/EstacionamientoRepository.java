bb200bfdf605c035853fe3d561a4c508c62d3d45
hectorforge <hector.hernandez.dev@gmail.com>
2025-06-11 20:32:17 -0500

    Repositorio

--- sistema-estacionamiento-main/src/main/java/com/estacionamiento/persistence/EstacionamientoRepository.java
+++ sistema-estacionamiento-main/src/main/java/com/estacionamiento/persistence/EstacionamientoRepository.java
@@ -1,5 +1,7 @@
 package com.estacionamiento.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.estacionamiento.entity.Estacionamiento;
 
 @Repository
 public interface EstacionamientoRepository extends JpaRepository<Estacionamiento, Integer>{
-	
+	List<Estacionamiento> findByActivoTrue();
 }
